6307e58a2a0563165c519b9af816a335589231ef
amitchrs8 <amitchrs8@gmail.com>
2022-02-28 23:23:48 +0530

    committing ds module

---
+++ venv/basics/python_ds/python_ds_ops.py
@@ -0,0 +1,14 @@
+print('this module is to test basic Python Data Structure  functionality')
+
+def test_list():
+    print('Testing List Ops')
+    li=range(10,40,5)
+    print(list(li))
+
+
+
+
+
+if __name__=="__main__":
+    print('Name of Module is : {}'.format(__name__))
+    test_list()
